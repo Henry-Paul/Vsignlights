3d108978940bab661d1e8577edc7d8e6b788f44d
Henry-Paul <164559529+Henry-Paul@users.noreply.github.com>
2026-01-03 13:17:46 +0530

    Update floatingactions.js

--- components/floatingactions.js
+++ components/floatingactions.js
@@ -1,8 +1,8 @@
 export default function FloatingActions() {
   return (
     <div className="floating">
-      <a href="tel:+91XXXXXXXXXX">📞</a>
       <a href="https://wa.me/91XXXXXXXXXX" target="_blank">💬</a>
+      <a href="tel:+91XXXXXXXXXX">📞</a>
     </div>
   );
 }
