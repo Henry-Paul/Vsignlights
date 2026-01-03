5d989754600faed08fdc4b1048062aedda971c1c
Henry-Paul <164559529+Henry-Paul@users.noreply.github.com>
2026-01-03 13:06:09 +0530

    Create floatingactions.js

---
+++ components/floatingactions.js
@@ -0,0 +1,8 @@
+export default function FloatingActions() {
+  return (
+    <div className="floating">
+      <a href="tel:+91XXXXXXXXXX">📞</a>
+      <a href="https://wa.me/91XXXXXXXXXX" target="_blank">💬</a>
+    </div>
+  );
+}
